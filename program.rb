0c39f5ec3d053d2b1272bc11a2e339f3753cb0ac
ritamritam <73716210+ritamritam@users.noreply.github.com>
2020-10-30 21:38:32 +0100

    Create program.rb

---
+++ program.rb
@@ -0,0 +1,15 @@
+
+arr = [6,2,8,1,3,9,15]
+
+n = arr.length
+
+for i in 0...n-1 do
+    for j in 0...n-i-1 do
+      if arr[j] > arr[j+1]
+        t = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = t
+      end
+    end
+end
+
